04b426f0d2f197b0bfae4b4919e6cab14b0fd4db
peeyushguleria <peeyushguleria@gmail.com>
2024-02-01 23:03:00 +0530

    added answer to loveYouOrNot.js file

--- loveYouOrNot.js
+++ loveYouOrNot.js
@@ -13,3 +13,16 @@ When the last petal was torn there were cries of excitement, dreams, surging tho
 Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0. */
 
 // Answer //
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
